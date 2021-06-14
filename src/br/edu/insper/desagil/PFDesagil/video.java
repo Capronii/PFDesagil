4efb757625f8296dbc9ae9fbdef518fbe83cbfae
Caproni <gcapronii@hotmail.com>
2021-06-14 15:07:56 -0300

    Preparando para prova

---
+++ src/br/edu/insper/desagil/PFDesagil/video.java
@@ -0,0 +1,5 @@
+package br.edu.insper.desagil.PFDesagil;
+
+public class video {
+
+}
---
+++ test/br/edu/insper/desagil/PFDesagil/Test_da_PF.java
@@ -0,0 +1,15 @@
+package br.edu.insper.desagil.PFDesagil;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test_da_PF {
+	
+
+	@Test
+	void test() {
+		asser.equals("")
+	}
+
+}
